7e7149dd10746d564b3d52924d4726a7e9ba1f33
Mauro OLSEN <molsen@viviendaverde.com>
2022-03-26 01:05:15 -0300

    Agregando Progressbar

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,13 +1,22 @@
 import { useState, useEffect } from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import "react-circular-progressbar/dist/styles.css"
 
 function ControlPresupuesto({gastos, presupuesto}) {
 
+  const [porcentaje, setPorcentaje] = useState(0)
   const [disponible, setDisponible] = useState(0)
   const [gastado, setGastado] = useState(0)
 
   useEffect(() => {
     const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0)
     const totalDisponible = presupuesto - totalGastado
+
+    const nuevoPorcentaje = (((presupuesto - totalDisponible) / presupuesto) * 100).toFixed(2)
+    setTimeout(() => {
+      setPorcentaje(nuevoPorcentaje)
+    }, 600)
+
     setDisponible(totalDisponible)
     setGastado(totalGastado)
   }, [gastos])
@@ -22,18 +31,26 @@ function ControlPresupuesto({gastos, presupuesto}) {
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
         <div>
-            <p>Grafica aqui</p>
-            <div className='contenido-presupuesto'>
-                <p>
-                    <span> Presupuesto: </span> {formatCantidad(presupuesto)}
-                </p>
-                <p>
-                    <span> Disponible: </span> {formatCantidad(disponible)}
-                </p>
-                <p>
-                    <span> Gastado: </span> {formatCantidad(gastado)}
-                </p>
-            </div>
+          <CircularProgressbar 
+            styles={buildStyles({
+              pathColor: '#3B82F6',
+              trailColor: '#F5F5F5',
+              textColor: '#3B82F6'
+            })}
+            value={porcentaje}
+            text={`${porcentaje}% Gastado`}
+          />
+        </div>
+        <div className='contenido-presupuesto'>
+            <p>
+                <span> Presupuesto: </span> {formatCantidad(presupuesto)}
+            </p>
+            <p>
+                <span> Disponible: </span> {formatCantidad(disponible)}
+            </p>
+            <p>
+                <span> Gastado: </span> {formatCantidad(gastado)}
+            </p>
         </div>
     </div>
   )
